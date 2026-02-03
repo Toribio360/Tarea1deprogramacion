1ef442e614318ccc6587d5a49193383de705c270
Toribio360 <20242551@itla.edu.do>
2026-02-02 22:25:55 -0400

    task2done

---
+++ homework2/homework2/Program.cs
@@ -0,0 +1,28 @@
+﻿
+// desarrollar un programa que le solicite al usuario un numero y el programa indique si es par o impar.
+
+try
+{
+    Console.Write("Please enter a whole number to determine if it is even or odd: ");
+
+
+    int number = Convert.ToInt32(Console.ReadLine());
+
+    if (number % 2 == 0)
+    {
+
+        Console.WriteLine($"The number: {number} is even.");
+
+    }
+    else
+    {
+        Console.WriteLine($"The number: {number} is odd.");
+        
+    }
+}
+catch (Exception)
+{
+
+  Console.WriteLine("Error: You must enter a whole number");
+}
+
